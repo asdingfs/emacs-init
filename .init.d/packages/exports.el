4e082104ad49f8afcd90dcb8f78e744156df95b3
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2016-10-24 06:11:54 +0800

    Add htmlize to beautify org exports, and beautify source display complying with zenburn theme definitions

--- .init.d/packages/exports.el
+++ .init.d/packages/exports.el
@@ -1,12 +1,25 @@
 ;; main org configurations
 (use-package org
-  :commands (ox-twbs)
+  :commands (ox-twbs htmlize)
   :defer t
   :init
-  (setq org-export-with-sub-superscripts nil))
+  (setq org-src-fontify-natively t)     ;; make source code block prettier
+  (setq org-export-with-sub-superscripts nil)
+  (defface org-block-begin-line
+    '((t (:background "#383838" :foreground "#7F9F7F")))
+    "Face used for the line delimiting the begin of source blocks.")
+  (defface org-block-background
+    '((t (:background "#383838")))
+    "Face used for the source block background.")
+  (defface org-block-end-line
+    '((t (:background "#383838" :foreground "#7F9F7F")))
+    "Face used for the line delimiting the end of source blocks.")  )
 
 ;; exporting
 (use-package ox-twbs ;; pretty html
+  :after (org)
+  :defer t)
+(use-package htmlize
   :after (org)
   :defer t)
 
