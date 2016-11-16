0e3abb5ce689d85d4d814f801d6390d168d8b292
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2016-11-16 17:32:05 +0800

    Add Shift keybindings for many smartparens keybinding to 'force' delete

--- .init.d/packages/editing.el
+++ .init.d/packages/editing.el
@@ -1,21 +1,47 @@
 ;; autopairs
+;; deletion, movement, modification
+;;    for deletions, if S is defined, force overwrite the sp-command (i.e. use the normal command)
+;;    for movements, if S is defined, use reverse direction
 (use-package smartparens
-  :bind
-  (("C-M-k" . sp-kill-sexp)
-   ("C-M-f" . sp-forward-sexp)
-   ("C-M-b" . sp-backward-sexp)
-   ("C-M-u" . sp-up-sexp)
-   ("C-M-d" . sp-down-sexp)
-   ("C-)" . sp-forward-slurp-sexp)
-   ("C-}" . sp-forward-barf-sexp)
-   ("C-(" . sp-backward-slurp-sexp)
-   ("C-{" . sp-backward-barf-sexp)) 
+  :bind (:map smartparens-mode-map
+              ("C-M-k" . sp-kill-sexp)
+              ("C-k" . sp-kill-hybrid-sexp)
+              ("C-S-k" . sp-backward-kill-sexp)
+              ("C-S-w" . sp-kill-region) 
+              ("C-w" . kill-region) 
+              ([remap sp-kill-region] . kill-region)
+              ("C-<backspace>" . sp-backward-kill-word)
+              ("M-<backspace>" . backward-kill-word)
+              ([remap sp-backward-kill-word] . backward-kill-word)
+              ("S-<backspace>" . sp-backward-delete-char)
+              ("<backspace>" . backward-delete-char)
+              ([remap sp-backward-delete-char] . backward-delete-char)
+              
+              ("C-M-a" . sp-beginning-of-sexp)
+              ("C-M-e" . sp-end-of-sexp)
+              ("C-S-f" . sp-forward-symbol)
+              ("C-M-f" . sp-forward-sexp)
+              ("C-S-b" . sp-backward-symbol)
+              ("C-M-b" . sp-backward-sexp)
+              ("C-M-n" . sp-next-sexp)
+              ("C-M-p" . sp-previous-sexp)
+              ("C-S-u" . sp-backward-up-sexp)
+              ("C-M-u" . sp-up-sexp)
+              ("C-S-d" . sp-backward-down-sexp)
+              ("C-M-d" . sp-down-sexp)
+
+              ("C-M-t" . sp-transpose-sexp)
+              ("M-[" . sp-backward-unwrap-sexp)
+              ("M-]" . sp-unwrap-sexp)
+              ("C-)" . sp-forward-slurp-sexp)
+              ("C-}" . sp-forward-barf-sexp)
+              ("C-(" . sp-backward-slurp-sexp)
+              ("C-{" . sp-backward-barf-sexp))
   :init
   (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
   :config
   (require 'smartparens-config)
-  (show-smartparens-global-mode t)
-  )
+  (show-smartparens-global-mode t))
 
 ;; snippets
 (use-package yasnippet
