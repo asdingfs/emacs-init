b87eb80fa2f02b7e264aac91cffda0409ac965aa
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2017-03-09 00:08:01 +0800

    Change keybindings of duplicate line to H-SPC

--- .init.d/keybindings.el
+++ .init.d/keybindings.el
@@ -14,7 +14,7 @@
 
 ;; custom key macros
 (global-set-key (kbd "H-c") 'copy-line)                                 ;; copy ARGS line or region if mark-active, successive calls appends to the same kill ring
-(global-set-key (kbd "C-c C-SPC") 'duplicate-line)                      ;; duplicate a current line
+(global-set-key (kbd "H-SPC") 'duplicate-line)                          ;; duplicate a current line
 (global-set-key (kbd "C-c C-<return>") (kbd "C-a C-j C-p TAB"))         ;; shift down line and begin with indentations
 (global-set-key (kbd "C-<return>") (kbd "C-e C-j"))                     ;; shift cursor down and make a new line
 
