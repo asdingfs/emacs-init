3c4e55751f37e10dffe7f70ce5efc61409b20640
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2017-05-31 15:56:18 +0800

    Use linum-relative package and enable linum-mode globally

--- .init.d/packages/movements.el
+++ .init.d/packages/movements.el
@@ -4,3 +4,12 @@
   (winner-mode 1))
 (global-set-key (kbd "H->") 'winner-redo)
 (global-set-key (kbd "H-<") 'winner-undo)
+
+;; linum-relative mode: display relative line numbers
+(use-package linum-relative
+  :init
+  (add-hook 'prog-mode-hook 'linum-relative-on)
+  :config
+  (setq linum-relative-current-symbol "=>"))
+;; enable linum mode globally
+(global-linum-mode t)
