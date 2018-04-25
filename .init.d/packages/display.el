93374a867b59e4fce329dd68a7fec4f717ce66b9
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2018-04-25 15:34:33 +0800

    Setup smart line mode

--- .init.d/packages/display.el
+++ .init.d/packages/display.el
@@ -16,3 +16,12 @@
 (setq column-number-mode t)
 
 ;; better mode-line
+(use-package smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'respectful)
+  (setq sml/name-width 40)
+  (setq sml/mode-width 'full)
+  (setq sml/modified-char "*")
+  (setq sml/show-client t)
+  (add-hook 'after-init-hook #'sml/setup))
