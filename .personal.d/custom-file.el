b6d36677d266f30e5831d17602625dddbbd201a1
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2018-05-02 09:48:52 +0800

    Set custom variable to always load projectile and counsel-projectile mode

--- .personal.d/custom-file.el
+++ .personal.d/custom-file.el
@@ -4,6 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(projectile-mode t nil (projectile))
+ '(counsel-projectile-mode t nil (counsel-projectile))
  '(custom-file "~/.emacs.d/.personal.d/custom-file.el")
  '(org-agenda-files
    (quote
