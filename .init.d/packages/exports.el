164ecc528fd1be7f66dca6e66acdb6152728fe78
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2018-09-04 09:09:36 +0800

    Set org imenu depth to 3

--- .init.d/packages/exports.el
+++ .init.d/packages/exports.el
@@ -21,6 +21,7 @@
   (setq org-log-done t)
   (setq org-goto-auto-isearch nil)
   (setq org-agenda-files (directory-files "~/.emacs.d/.personal.d/org" t "^.*\\.org$"))
+  (setq org-imenu-depth 3)
   (setq org-src-window-setup 'current-window)
   ;; refiling
   (setq org-refile-targets (quote ((nil :maxlevel . 3)
