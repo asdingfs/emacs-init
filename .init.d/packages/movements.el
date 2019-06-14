24c5c326f04a78e1c94a2fdcd39031c9da3e30ff
Anthony Setiawan <asdingfs@Anthonys-MacBook-Pro.local>
2019-06-14 14:40:12 +0800

    Add buffer-move and desktop keybindings

--- .init.d/packages/movements.el
+++ .init.d/packages/movements.el
@@ -1,13 +1,28 @@
 ;;;;;;;;;;;;;;;; temporal ;;;;;;;;;;;;;;;;
 ;; winner mode: splitting configurations
 (when (fboundp 'winner-mode)
-  (winner-mode 1))
-(global-set-key (kbd "H->") 'winner-redo)
-(global-set-key (kbd "H-<") 'winner-undo)
+  (winner-mode 1)
+  (global-set-key (kbd "H->") 'winner-redo)
+  (global-set-key (kbd "H-<") 'winner-undo))
+(when (fboundp 'desktop-save-mode)
+  (desktop-save-mode 0)
+  (global-set-key (kbd "H-\`") 'desktop-read)
+  (global-set-key (kbd "H-\~") 'desktop-save-in-desktop-dir)
+  (add-to-list 'desktop-path "~/.emacs.d/.personal.d/locals/")
+  (setq desktop-dirname "~/.emacs.d/.personal.d/locals/"))
 
 ;; transpose-frame to switch windows
 (use-package transpose-frame
   :bind
-  (("H-\"" . flop-frame)
-   ("H-?" . flip-frame)
+  (("H-}" . flop-frame)
+   ("H-|" . flip-frame)
    ("C-x \\" . transpose-frame)))
+
+;; buffer-move to move windows directionally
+(use-package buffer-move
+  :bind
+  (("H-:" . buf-move-left)
+   ("H-\"" . buf-move-right)
+   ("H-{" . buf-move-up)
+   ("H-?" . buf-move-down)))
+
--- .personal.d/custom-file.el
+++ .personal.d/custom-file.el
@@ -10,7 +10,7 @@
  '(flycheck-javascript-flow-args nil)
  '(package-selected-packages
    (quote
-    (rubocop multiple-cursors flycheck-flow flycheck flow-minor-mode hippie-expand react-snippets yasnippet-snippets rjsx-mode company xref-js2 js2-refactor js2-mode git-timemachine counsel swiper ivy projectile smart-mode-line diminish nlinum-relative linum-relative pandoc-mode wgrep dumb-jump magit-gitflow transpose-frame w3m markdown-preview-mode origami expand-region json-mode markdown-mode ox-pandoc highlight-indent-guides web-mode rvm gitignore-mode git-modes magit ag rspec-mode yasnippet ssh-config-mode ssh yaml-mode htmlize org-bullets dash-at-point ox-twbs smartparens enh-ruby-mode projectile-rails counsel-projectile flx exec-path-from-shell helm base16-theme zenburn-theme use-package))))
+    (buffer-move rubocop multiple-cursors flycheck-flow flycheck flow-minor-mode hippie-expand react-snippets yasnippet-snippets rjsx-mode company xref-js2 js2-refactor js2-mode git-timemachine counsel swiper ivy projectile smart-mode-line diminish nlinum-relative linum-relative pandoc-mode wgrep dumb-jump magit-gitflow transpose-frame w3m markdown-preview-mode origami expand-region json-mode markdown-mode ox-pandoc highlight-indent-guides web-mode rvm gitignore-mode git-modes magit ag rspec-mode yasnippet ssh-config-mode ssh yaml-mode htmlize org-bullets dash-at-point ox-twbs smartparens enh-ruby-mode projectile-rails counsel-projectile flx exec-path-from-shell helm base16-theme zenburn-theme use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
