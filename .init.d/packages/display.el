9c6e46c768a06eaba6e580a28911251dcbe8c140
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2020-04-08 15:01:01 +0800

    Enable and configure whitespace mode

--- .init.d/packages/display.el
+++ .init.d/packages/display.el
@@ -15,3 +15,20 @@
   (setq sml/modified-char "*")
   (setq sml/show-client t)
   (add-hook 'after-init-hook #'sml/setup))
+
+;; whitespace
+(use-package whitespace
+  :config
+  ;; enable whitespace mode globally
+  (global-whitespace-mode t)
+  ;; customise display of whitespaces
+  (setq whitespace-display-mappings
+        (quote ((space-mark 32 [?·]) ;; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+                (newline-mark 10 [182 10]) ;; 10 LINE FEED to 「¶」
+                (tab-mark 9 [9655 9] [92 9])))) ;; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+  (setq whitespace-style (quote (face trailing spaces tabs newline newline space-mark tab-mark newline-mark)))
+  (set-face-attribute 'whitespace-space nil :background 'unspecified :foreground "gray30")
+  ;; since font breaks for Magit, disable for Magit buffers
+  (defun prevent-whitespace-mode-for-magit ()
+    (not (derived-mode-p 'magit-mode)))
+  (add-function :before-while whitespace-enable-predicate 'prevent-whitespace-mode-for-magit))
