8907f74b7169422df3f9ff6edebb0b464c68f5de
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2020-05-06 00:02:46 +0800

    Fix AppleScript issue temporarily

--- .init.d/specifics.el
+++ .init.d/specifics.el
@@ -8,7 +8,7 @@
 (when (featurep 'ns)
   (defun ns-raise-emacs ()
     "Raise Emacs."
-    (ns-do-applescript "tell application \"Emacs\" to activate"))
+    (select-frame-set-input-focus (selected-frame)))
 
   (defun ns-raise-emacs-with-frame (frame)
     "Raise Emacs and select the provided frame."
