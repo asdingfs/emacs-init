d7a89b030a2e48ae5c69c172c71e606edb49b02d
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2020-06-17 05:13:15 +0800

    Addd thesaurus to emacs

--- .init.d/packages.el
+++ .init.d/packages.el
@@ -2,7 +2,8 @@
   (dolist (filename '(display ;; for display enhancements to emacs
                       exports ;; most of org-mode configurations here
                       editing ;; aids editing of texts/codes
+                      writing ;; configs for writing modes and environments
                       programming ;; configs for programming modes and environments
                       finders ;; finders for things you seek (projectile, ivy, etc)
-                      movements)) ;; movements, temporal and spatial                    
+                      movements)) ;; movements, temporal and spatial
     (load (format "%s%s" directory filename)))) ;; load each packages declared above
---
+++ .init.d/packages/writing.el
@@ -0,0 +1,4 @@
+;; define-word
+(use-package mw-thesaurus
+  :bind
+  (("C-c t" . mw-thesaurus-lookup-at-point)))
