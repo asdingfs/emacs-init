900047948dbd81dc69a5bd806112b6dd61ab8a3b
unknown <anthonysetiawan.ding@gmail.com>
2021-05-05 14:53:59 +0800

    Use git bash in Windows and attempt to explicitly state that alt should not be sent to system

--- .init.d/specifics.el
+++ .init.d/specifics.el
@@ -45,9 +45,15 @@
 (when (featurep 'w32)
   ;; modifier keys
   (setq w32-pass-rwindow-to-system nil)
+  (setq w32-pass-alt-to-system nil)
   (setq w32-rwindow-modifier 'super) ;; Right Windows keys
   ;; NOTE: need to set the following Autohotkey Configuration
   ;; Also, turn on FILTER KEYS in Ease Access>Keyboard
   (bind-key "<f24>" 'event-apply-hyper-modifier function-key-map);;
   (w32-register-hot-key [s-])
-  (w32-register-hot-key [H-]))
+  (w32-register-hot-key [H-])
+
+  (setq shell-file-name "C:/Program Files/Git/bin/bash.exe")
+  (setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
+  (setenv "SHELL" shell-file-name)
+  (setenv "ESHELL" shell-file-name))
