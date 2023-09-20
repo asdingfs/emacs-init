4891ad58d4677a9c20dcc7e3bcf3ec4bd4babccf
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2023-09-21 01:39:46 +0800

    Style and beautify org-mode with padding and org-modern

--- .init.d/packages/org.el
+++ .init.d/packages/org.el
@@ -25,29 +25,48 @@
   :init
   (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
   (add-hook 'org-mode-hook (lambda () (org-cycle-hide-drawers 'all)))
+  (add-hook 'org-mode-hook (lambda () (org-indent-mode 't)))
+  (add-hook 'org-mode-hook (lambda () (visual-line-mode 't)))
+  (add-hook 'org-mode-hook (lambda () (setq line-spacing 0.15)))
   :config
   ;; general
   (org-all-files-refresh)
   (org-agenda-files-refresh)
-  (setq org-pretty-entities t)
-  (setq org-hide-leading-stars t)
+  (setq org-pretty-entities 't)
+  (setq org-hide-leading-stars 't)
   (setq org-ellipsis "⤵")
   (let* ((serif-font-family '(:family "Baskerville"))
-         (sans-serif-font-family '(:family "Iosevka Aile"))
+         (sans-serif-font-family '(:family "Avenir" :weight regular))
          (monospaced-font-family '(:family "Iosevka Term"))
+         (variable-tuple `(,@sans-serif-font-family))
          (headline `(,@monospaced-font-family)))
     (custom-theme-set-faces
      'user
-     `(org-level-8 ((t (,@headline :foreground "bisque3"))))
-     `(org-level-7 ((t (,@headline :foreground "bisque3"))))
-     `(org-level-6 ((t (,@headline))))
-     `(org-level-5 ((t (,@headline))))
-     `(org-level-4 ((t (,@headline))))
-     `(org-level-3 ((t (,@headline))))
-     `(org-level-2 ((t (,@headline))))
-     `(org-level-1 ((t (,@headline))))
-     `(org-document-title ((t (:height 1.2))))
+     `(org-level-8 ((t (,@headline ,@variable-tuple :height 1 :foreground "bisque3"))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple :height 1 :foreground "bisque3"))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple :height 1))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.2))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.3))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.4))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :weight medium))))
      ))
+  (set-face-attribute 'default nil :family "Iosevka Term")
+  (set-face-attribute 'fixed-pitch nil :family "Iosevka Term")
+  (set-face-attribute 'variable-pitch nil :family "Avenir")
+  (custom-theme-set-faces
+   'user
+   '(org-block ((t (:inherit fixed-pitch))))
+   '(org-code ((t (:inherit (shadow fixed-pitch)))))
+   '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+   '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-property-value ((t (:inherit fixed-pitch))) t)
+   '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-table ((t (:inherit fixed-pitch))))
+   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold))))
+   '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
   (setq org-cycle-level-faces nil)
   (setq org-cycle-hide-drawer-startup nil)
   (setq org-hide-emphasis-markers t) ;; show content only
@@ -143,8 +162,16 @@
   (setq org-export-with-sub-superscripts nil)
   (setq org-export-backends '(ascii html icalendar latex odt md))
   ;; looks
+  (setq org-indent-indentation-per-level 2) ;; set indentation level to 2 (avoid Iosevka Term's compactness)
+  (setq org-adapt-indentation nil)
+  (customize-set-variable 'org-blank-before-new-entry
+                          '((heading . nil)
+                            (plain-list-item.nil)))
+  (setq org-cycle-separator-lines 1)
   (setq org-src-fontify-natively t)     ;; make source code block prettier
   (setq org-src-tab-acts-natively t)    ;; make tab works as if programming normally
+  (setq org-src-preserve-indentation t) ;; preserve leading whitespace character in code blocks
+  (setq org-edit-src-content-indentation nil) ;; this has no effect if org-src-preserve-indentation is non-nil
   (defface org-block-begin-line
     '((t (:background "#383838" :foreground "#7F9F7F")))
     "Face used for the line delimiting the begin of source blocks.")
@@ -238,10 +265,16 @@
   (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
 
 (use-package org-modern
-  :config
+  :init
   (add-hook 'org-mode-hook #'org-modern-mode)
   (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
-  )
+  :config
+  (setq
+   org-modern-checkbox nil
+   org-modern-list nil
+   org-modern-todo nil
+   org-modern-tag nil
+   org-modern-block-fringe 0))
 
 (use-package org-download ;; drag and drop file into emacs buffer & upload as attachments
   :config
