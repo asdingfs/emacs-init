b8f659f7e869ccbf36ad7ca7e46d9ad3772111a3
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2023-09-21 01:56:42 +0800

    Sync automatic setting changes done by emacs

--- .personal.d/custom-file.el
+++ .personal.d/custom-file.el
@@ -25,7 +25,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.5 :weight medium))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.5))))
+ '(org-level-2 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.4))))
+ '(org-level-3 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.3))))
+ '(org-level-4 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.2))))
+ '(org-level-5 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1.1))))
+ '(org-level-6 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1))))
+ '(org-level-7 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1 :foreground "bisque3"))))
+ '(org-level-8 ((t (:family "Iosevka Term" :family "Avenir" :weight regular :height 1 :foreground "bisque3"))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 
 ;;;;;;;;;;;;;;;; Disabled warning commands ;;;;;;;;;;;;;;;;
 (put 'narrow-to-region 'disabled nil)
