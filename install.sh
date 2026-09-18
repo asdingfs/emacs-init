14f543d9b128827ba6a61803cb0f2280a867385b
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2026-09-18 16:56:55 +0800

    New utility tool in Cask installs

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 # packages to install in this script
 BREW_PACKAGES=(wget curl gpg z ripgrep ag w3m pandoc git python postgres redis yt-dlp awscli)
 # TODO: additional brew packages: texinfo
-CASK_PACKAGES=(1password paragon-ntfs onyx appcleaner emacs iterm2 karabiner-elements font-inconsolata font-latin-modern-math dropbox firefox telegram discord zoom tunnelblick spotify postman docker android-file-transfer android-studio vysor google-chrome blender figma gimp inkscape musicbrainz-picard pdf-expert musescore calibre vlc reflector duet parsec jump-desktop jump-desktop-connect steam openemu transmission alfred)
+CASK_PACKAGES=(1password paragon-ntfs onyx appcleaner emacs iterm2 karabiner-elements font-inconsolata font-latin-modern-math dropbox firefox telegram discord zoom tunnelblick spotify postman docker android-file-transfer android-studio vysor google-chrome blender figma gimp inkscape musicbrainz-picard pdf-expert musescore calibre vlc reflector duet parsec jump-desktop jump-desktop-connect steam openemu transmission alfred rectangle-pro calendar-366 clocker)
 
 # apps to install manually:
 # 2. Download from AppStore:
